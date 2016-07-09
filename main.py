af623ffcb114eced4ec572665e8127771563ac5c
Veljko Simic <veljko.simic98@gmail.com>
2016-07-09 23:57:12 +0200

    Select roi

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
 from matplotlib import pyplot as plt
-from preprocessing_image import image_gray, image_bin_adaptive, image_otsu_treshold, remove_noise, closing
-PATH = './images/test2.jpg'
+import numpy as np
+from preprocessing_image import image_gray, image_bin_adaptive, image_otsu_treshold, remove_noise, closing, dilate, invert, select_roi, image_bin
+PATH = './images/tests/test10.png'
 
 def load_image(path):
     return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
@@ -11,15 +12,14 @@ def display_image(image):
     plt.show()
 
 def main():
+
     img_core = load_image(PATH)
-    img = image_bin_adaptive(image_gray(img_core))
-    img = image_otsu_treshold(img)
-    cimg = closing(img)
-    plt.subplot(121)
-    plt.imshow(img, 'gray')
-    plt.subplot(122)
-    plt.imshow(cimg, 'gray')
-    plt.show()
+    train_bin = image_bin(image_gray(img_core))
+    train_bin = invert(train_bin)
+    train_bin = dilate(train_bin, 1)
+    display_image(train_bin)
+    selected_regions, component = select_roi(img_core.copy(), train_bin)
+    display_image(selected_regions)
 
 if __name__ == "__main__":
     main()
--- preprocessing_image.py
+++ preprocessing_image.py
@@ -1,9 +1,14 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import collections
 
 def image_gray(image):
     return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+def image_bin(image_gs):
+    ret,image_bin = cv2.threshold(image_gs, 130, 255, cv2.THRESH_BINARY)
+    return image_bin
 
 def image_bin_adaptive(image_gs):
     image_bin = cv2.adaptiveThreshold(image_gs, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 35, 10)
@@ -16,7 +21,7 @@ def image_otsu_treshold(img):
 
 def dilate(image, value):
     kernel = np.ones((value,value))
-    return cv2.dilate(image, kernel, iterations=1)
+    return cv2.dilate(image, kernel, iterations=3)
 
 def erode(image, value):
     kernel = np.ones((value,value))
@@ -26,13 +31,35 @@ def invert(image):
     return 255-image
 
 def remove_noise(binary_image):
-    ret_val = erode(dilate(binary_image, 2), 3)
-    ret_val = invert(ret_val)
     ret_val = erode(dilate(binary_image, 3), 3)
     return ret_val
 
 def closing(binary_image):
     ret_val = dilate(erode(binary_image, 3), 3)
-    ret_val = invert(ret_val)
-    ret_val = dilate(erode(binary_image, 3), 3)
     return ret_val
+
+def select_roi(image_orig, image_bin):
+
+    im2, contours, hierarchy = cv2.findContours(image_bin.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    sorted_regions = []
+    regions_dic = {}
+    region_borders = []
+    i = -1
+
+    plt.imshow(im2)
+    plt.show()
+
+    for contour in contours:
+        x,y,w,h = cv2.boundingRect(contour)
+        print('here')
+        borders = [x,y,w,h]
+        region_borders.append(borders)
+        region = image_bin[y:y+h+1,x:x+w+1]
+        regions_dic[x] = region
+        cv2.rectangle(image_orig,(x-3,y-3),(x+w+3,y+h+3),(255,0,0),1)
+
+    sorted_regions_dic = collections.OrderedDict(sorted(regions_dic.items()))
+    sorted_regions = sorted_regions_dic.values()
+
+    return image_orig, sorted_regions
