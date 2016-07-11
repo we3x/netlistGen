40ab850e337de5dd41870cf69a8ead0693f141ed
Veljko Simic <veljko.simic98@gmail.com>
2016-07-11 16:23:38 +0200

    Merage components

--- preprocessing_image.py
+++ preprocessing_image.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import collections
+from operator import itemgetter
 
 def image_gray(image):
     return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -38,6 +39,36 @@ def closing(binary_image):
     ret_val = dilate(erode(binary_image, 3), 3)
     return ret_val
 
+def merage_contours(borders):
+    region_borders = []
+    for i in range(len(borders)):
+        x1, y1, w1, h1 = borders[i]
+        for j in range(len(borders)):
+            x2, y2, w2, h2 = borders[j]
+            if(x1< x2 and x1 + w1 > x2 and y1 + h1 > y2 and y1<y2):
+                w1 = x2 - x1 + w2
+                borders[j] = [0,0,0,0]
+        borders[i] = x1,y1,w1,w2
+
+    for border in borders:
+        if (border != [0,0,0,0]):
+            region_borders.append(border)
+
+    return region_borders
+
+def remove_max(borders):
+    surfices = []
+    ret_borders = []
+    for x,y,w,h in borders:
+        surfices.append((x+w)*(y+h))
+
+    max_sur = max(surfices)
+    for x,y,w,h in borders:
+        if (((x+w)*(y+h)) != max_sur):
+            ret_borders.append([x,y,w,h])
+
+    return ret_borders
+
 def select_roi(image_orig, image_bin):
 
     im2, contours, hierarchy = cv2.findContours(image_bin.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -45,19 +76,23 @@ def select_roi(image_orig, image_bin):
     sorted_regions = []
     regions_dic = {}
     region_borders = []
-    i = -1
-
-    plt.imshow(im2)
-    plt.show()
 
     for contour in contours:
         x,y,w,h = cv2.boundingRect(contour)
-        print('here')
-        borders = [x,y,w,h]
+        x = x - 6
+        y = y - 6
+        w = w + 12
+        h = h + 12
+        borders = (x,y,w,h)
         region_borders.append(borders)
-        region = image_bin[y:y+h+1,x:x+w+1]
+
+    region_borders = remove_max(region_borders)
+    region_borders = merage_contours(region_borders)
+    for x,y,w,h in region_borders:
+        region = image_bin[y:y+h,x:x+w]
         regions_dic[x] = region
-        cv2.rectangle(image_orig,(x-3,y-3),(x+w+3,y+h+3),(255,0,0),1)
+        cv2.rectangle(image_orig,(x,y),(x+w,y+h),(255,0,0),1)
+
 
     sorted_regions_dic = collections.OrderedDict(sorted(regions_dic.items()))
     sorted_regions = sorted_regions_dic.values()
