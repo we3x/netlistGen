e55d91fc5d814814c59338942e5d77bc954d8419
Veljko Simic <veljko.simic98@gmail.com>
2016-07-13 19:31:25 +0200

    Resolve spaces

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
-from preprocessing_image import image_gray, image_bin_adaptive, image_otsu_treshold, remove_noise, closing, dilate, invert, select_roi, image_bin
+from preprocessing_image import image_bin, image_gray, invert, dilate, select_roi
+from recognize import EigenComponentModel
 PATH = './images/tests/test5.png'
 
 def load_image(path):
@@ -19,15 +20,14 @@ def main():
         img = load_image('./images/components/'+label+'.png')
         train_images.append(img)
 
-    for img in train_images:
-        display_image(img)
-
-
     img_core = load_image(PATH)
     img_core_bin = image_bin(image_gray(img_core))
     img_core_invert = invert(img_core_bin)
     img_core_dilate = dilate(img_core_invert, 1)
     img_core_selected, component, regions = select_roi(img_core.copy(), img_core_dilate)
+
+    eigenModel = EigenComponentModel(train_images, labels)
+
     display_image(img_core_selected)
 
 if __name__ == "__main__":
--- recognize.py
+++ recognize.py
@@ -1,144 +1,70 @@
 import cv2
 import numpy as np
 
-
-class TooHigh(Exception):
-
-	def __init__(self):
-		pass
+class TooHeigh(Exception):
+    def __init__(self):
+        pass
 
 class EigenComponentModel(object):
-	"""
-	Class for Eigen Component model
-	"""
 
-	def __init__(self, images, labels):
-		"""
-		Initialization and training
+    def __init__(self, images, labels):
+        self.images = images
+        self.labels = labels
+        self.model = cv2.face.createEigenFaceRecognizer()
+        self.model.train(images, np.array(labels))
 
-		images: training set of images
-		labels: labels for training images
-		"""
-		self.images = images
-		self.labels = labels
-		self.model = cv2.createEigenFaceRecognizer()
-		self.model.train(images, np.array(labels))
+    def get_cv2_EigenComponentRecognizer(self):
+        return self.model
 
-	def get_cv2_EigenComponentRecognizer(self):
-		return self.model
+    def get_mean(self):
+        mean = self.model.getMat("mean")
+        return mean.reshape(mean.shape[1])
 
-	def get_mean(self):
-		"""
-		Returns mean image as width*height-dimensional vector
+    def get_eigenvectors(self):
+        vectors = self.model.getMat("eigenvectors")
+        return vectors.reshape(vectors.shape[0], vectors.shape[1].transponse())
 
-		returns: numpy array which represents image
-		"""
-		mean = self.model.getMat("mean")
-		return mean.reshape(mean.shape[1])
+    def project_component(self, component):
+        mean = self.get_mean()
+        component = reshape_component(component)
+        vectors = self.get_eigenvectors()
+        c = []
+        for vector in vectors:
+            c.append(vector.dot(component - mean))
+        return np.array(c)
 
-	def get_eigenvectors(self):
-		"""
-		Returns numpy array of vectors which represent eigen vectors
+    def get_scores(self, component, treshold, training=None):
+        distances = []
+        f = self.project_component(component)
+        if training == None:
+            training = self.images
+        for img in training:
+            t = self.project_component(img)
+            distances.append(np.sqrt(np.sum((f-t)**2)))
+        scores = []
+        for distance in distances:
+            if distance >= treshold:
+                distance = treshold -1
+            scores.append(np.log(treshold - distances) - np.log(treshold))
+        return scores
 
-		returns: eigenvectors (unit vectors)
-		"""
-		vectors = self.model.getMat("eigenvectors")
-		return vectors.reshape(vectors.shape[0], vectors.shape[1]).transpose()
+    def score(self, component, ttt, treshold):
+        f = self.project_component(component)
+        t = self.project_component(ttt)
+        d = np.sqrt(np.sum((f-t)**2))
+        if d >= treshold:
+            d = treshold - 1
+        return np.log(treshold-d) - np.log(treshold)
 
-
-	def project_component(self, component):
-		"""
-		Projects component into eigenspace
-
-		component: component image to be projected
-
-		returns: projected vector (component)
-		"""
-		mean = self.get_mean()
-		#print component.shape
-		component = reshape_component(component)
-		vectors = self.get_eigenvectors()
-		c = []
-		for v in vectors:
-			print "Component sha[e " + str(component.shape)
-			print "mean shape " + str(mean.shape)
-			c.append(v.dot(component - mean))
-		return np.array(c)
-
-	def get_scores(self, component, threshold, training=None):
-		"""
-		Get all scores for given component
-
-		component: width*height-dimensional vector which represents component
-		training: training set of images (reshaped)
-
-		returns: list of scores for each image in training set. NOTE: list is ordered like images in training set
-		"""
-		distances = []
-		f = self.project_component(component)
-		if training == None:
-			training = self.images
-		for img in training:
-			t = self.project_component(img)
-			distances.append(np.sqrt(np.sum((f-t)**2)))
-		scores = []
-		for d in distances:
-			if d >= threshold:
-				d = threshold - 1
-			scores.append(np.log(threshold - d) - np.log(threshold))
-		return scores
-
-	def score(self, component, ttt, threshold):
-		"""
-		Scores component from training set
-
-		component: reshaped vector which represents component
-		ttt: component to compare with (as images/matrices)
-
-		returns: score (logarithm of probability)
-
-		"""
-		f = self.project_component(component)
-		t = self.project_component(ttt)
-		print "Calculating distance...."
-		d = np.sqrt(np.sum((f-t)**2))
-		print "Distance: " + str(d)
-		if d >= threshold:
-			d = threshold - 1
-		return np.log(threshold - d) - np.log(threshold)
-
-	def score_projected(self, f, t, threshold):
-		"""
-		Scores projected component from training set of projected components
-
-		f: projected component (using project_component(...) function)
-		t: component to compare with (projected)
-
-		returns: score (logarithm of probability)
-
-		"""
-		d = np.sqrt(np.sum((f-t)**2))
-		if d >= threshold:
-                        raise TooHigh
-			d = threshold - 1
-		return np.log(threshold - d) - np.log(threshold)
-
-
-def load_component(path):
-	"""
-	Loads component as matrix
-
-	returns: Matrix which represents grayscale loaded images (component)
-	"""
-	return cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    def score_projected(self, f, t, treshold):
+        d = np.sqrt(np.sum((f-t)**2))
+        if d >= treshold:
+            raise TooHeigh
+            d = treshold - 1
+        return np.log(treshold - d) - np.log(treshold)
 
 def reshape_component(component):
-	"""
-	Reshapes component as array
-
-	returns: reshaped numpy.array (r*c)
-	"""
-	if len(component.shape) == 1:
-		return component
-	r,c = component.shape
-	return component.reshape(r*c)
+    if len(component.shape) == 1:
+        return component
+    r,c = component.shape
+    return component.reshape(r*c)
