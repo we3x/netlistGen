ebba2ee8619078437275c080ad1c53ca5b6a6ffd
Veljko Simic <veljko.simic98@gmail.com>
2016-07-13 18:08:38 +0200

    Load training images

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 from preprocessing_image import image_gray, image_bin_adaptive, image_otsu_treshold, remove_noise, closing, dilate, invert, select_roi, image_bin
-PATH = './images/tests/test10.png'
+PATH = './images/tests/test5.png'
 
 def load_image(path):
     return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
@@ -12,14 +12,23 @@ def display_image(image):
     plt.show()
 
 def main():
+    train_images = []
+    labels = ['and', 'nand', 'nor', 'not', 'xnor', 'or', 'xor']
+
+    for label in labels:
+        img = load_image('./images/components/'+label+'.png')
+        train_images.append(img)
+
+    for img in train_images:
+        display_image(img)
+
 
     img_core = load_image(PATH)
-    train_bin = image_bin(image_gray(img_core))
-    train_bin = invert(train_bin)
-    train_bin = dilate(train_bin, 1)
-    display_image(train_bin)
-    selected_regions, component = select_roi(img_core.copy(), train_bin)
-    display_image(selected_regions)
+    img_core_bin = image_bin(image_gray(img_core))
+    img_core_invert = invert(img_core_bin)
+    img_core_dilate = dilate(img_core_invert, 1)
+    img_core_selected, component, regions = select_roi(img_core.copy(), img_core_dilate)
+    display_image(img_core_selected)
 
 if __name__ == "__main__":
     main()
