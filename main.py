f5642ff1a73db4a7e1c810f1033b0227eeef2a7b
Veljko Simic <veljko.simic98@gmail.com>
2016-07-14 22:32:49 +0200

    resize training component

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 from preprocessing_image import image_bin, image_gray, invert, dilate, select_roi
-from recognize import EigenComponentModel
+from recognize import EigenComponentModel, reshape_component
 PATH = './images/tests/test5.png'
 
 def load_image(path):
@@ -15,9 +15,11 @@ def display_image(image):
 def main():
     train_images = []
     labels = ['and', 'nand', 'nor', 'not', 'xnor', 'or', 'xor']
+    train_labels = [0, 1, 2, 3, 4, 5, 6]
 
     for label in labels:
         img = load_image('./images/components/'+label+'.png')
+        img = cv2.resize(img, (128, 128))
         train_images.append(img)
 
     img_core = load_image(PATH)
@@ -26,7 +28,7 @@ def main():
     img_core_dilate = dilate(img_core_invert, 1)
     img_core_selected, component, regions = select_roi(img_core.copy(), img_core_dilate)
 
-    eigenModel = EigenComponentModel(train_images, labels)
+    eigenModel = EigenComponentModel(train_images, train_labels)
 
     display_image(img_core_selected)
 
